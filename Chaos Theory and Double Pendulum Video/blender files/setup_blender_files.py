7fa82288b2a9321653f8e87f4c606b6f5bf97856
Andrew Tres Reyes <drewscampfire@gmail.com>
2025-11-18 15:50:59 +0800

    added setup_blender_files.py

---
+++ Chaos Theory and Double Pendulum Video/blender files/setup_blender_files.py
@@ -0,0 +1,22 @@
+import sys
+
+
+filename = r"D:\Chaos-Theory\Andrew's Campfire Code for Videos"
+if filename not in sys.path:
+    sys.path.append(filename)
+
+filename = r"D:\Chaos-Theory\Andrew's Campfire Code for Videos\Chaos Theory and Double Pendulum Video\blender files"
+if filename not in sys.path:
+    sys.path.append(filename)
+
+filename = r"D:\Chaos-Theory\Andrew's Campfire Code for Videos\Chaos Theory and Double Pendulum Video"
+if filename not in sys.path:
+    sys.path.append(filename)
+
+filename = r"D:\Chaos-Theory\Andrew's Campfire Code for Videos\Chaos Theory and Double Pendulum Video\manim files"
+if filename not in sys.path:
+    sys.path.append(filename)
+
+filename = r"c:\users\andrew tres reyes\appdata\local\programs\python\python311\lib\site-packages"
+if filename not in sys.path:
+    sys.path.append(filename)
